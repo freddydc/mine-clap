bd219b71e3117b582bb1fcba7272fadbf5c3a41a
freddydc <freddydiaz.liv@gmail.com>
2022-06-14 11:42:15 -0500

    Handling states

--- src/components/TodoSearch/TodoSearch.jsx
+++ src/components/TodoSearch/TodoSearch.jsx
@@ -1,19 +1,28 @@
+import { useState } from 'react'
 import styles from './TodoSearch.module.css'
 
 export const TodoSearch = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+
   const handleSubmit = e => {
     e.preventDefault()
   }
 
   const searchValue = e => {
     console.log(e.target.value)
+    setSearchTerm(e.target.value)
   }
 
-  return (
-    <div className={styles.container}>
+  return [
+    <div key="2-4" className={styles.container}>
       <form onSubmit={handleSubmit}>
-        <input placeholder="Claps..." onChange={searchValue} />
+        <input
+          placeholder="Claps..."
+          value={searchTerm}
+          onChange={searchValue}
+        />
       </form>
-    </div>
-  )
+    </div>,
+    <p key="8-3">{searchTerm}</p>
+  ]
 }
